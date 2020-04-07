211148c1f8950b899654cd4f080882b413582e42
Priscila Bey <puri@Priscilas-MacBook-Pro.local>
2020-04-06 21:39:31 -0300

    Docs: Ej1

--- src/tp1/Ej1.java
+++ src/tp1/Ej1.java
@@ -1,5 +1,12 @@
 package tp1;
 
+/*
+EJ 1
+Realizá un programa donde se cree una variable entera llamada num1, que inicialmente valdrá 0.
+Luego incrementá su valor en 2 y mostralo por pantalla.
+Después mostrá el resultado de multiplicarlo por sí mismo.
+*/
+
 public class Ej1 {
     public static void main (String [] args) {
         int num1 = 0;
