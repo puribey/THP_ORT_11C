d64714310c7d25e8dfac9fe9c285b980ffd7d430
Priscila Bey <puri@Priscilas-MacBook-Pro.local>
2020-04-06 21:37:16 -0300

    Ej1

--- src/tp1/Ej1.java
+++ src/tp1/Ej1.java
@@ -1,23 +1,10 @@
 package tp1;
 
 public class Ej1 {
-    private static Scanner input;
     public static void main (String [] args) {
-        input = new Scanner (System.in);
-        int num1;
-        int num2;
-        System.out.println("Inserte primer número");
-        num1= input.nextInt();
-        input.nextLine();
-        System.out.println("Inserte otro número");
-        num2= input.nextInt();
-        input.nextLine();
-        if (num2!=0) {
-            double div = num1/num2;
-            System.out.println(num1+ "/" +num2 + "= " +div);
-        } else {
-            System.out.println("El segundo num es 0, no se puede dividir");
-        }
-        input.close();
+        int num1 = 0;
+        num1 = 2;
+        System.out.println("Num 1 vale: " + num1);
+        System.out.println("Num 1 x el mismo: " + (num1 * num1));
     }
 }
