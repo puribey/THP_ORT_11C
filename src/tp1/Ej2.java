c939c4c87e98fed7a46aac362773715fc75f6c93
Priscila Bey <puri@Priscilas-MacBook-Pro.local>
2020-04-06 22:41:27 -0300

    Ej2

---
+++ src/tp1/Ej2.java
@@ -0,0 +1,20 @@
+package tp1;
+
+/*
+EJ 2
+
+Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5, respectivamente,
+realice la operación correspondiente y muestre el resultado en pantalla:
+
+a. Suma
+b. Producto
+*/
+
+public class Ej2 {
+    public static void main (String [] args) {
+        int num1 = 4;
+        int num2 = 5;
+        System.out.println("Suma de num1 y num2: " + (num1 + num2));
+        System.out.println("Producto de num1 y num2: " + (num1 * num1));
+    }
+}
